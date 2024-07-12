cde104901ae9b6f59ad93de3563643fdd174e960
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-07-12 20:19:24 -0300

    chore: svg importer

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import {
-  useFonts,
   Urbanist_400Regular,
   Urbanist_500Medium,
   Urbanist_700Bold,
+  useFonts,
 } from '@expo-google-fonts/urbanist';
-import theme from '@/general/theme';
 
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+import Logo from './src/app/assets/icons/logo.svg';
 export default function App() {
   let [fontsLoaded] = useFonts({
     Urbanist_400Regular,
@@ -21,6 +22,10 @@ export default function App() {
 
   return (
     <View>
+      <Logo
+        width={100}
+        height={100}
+      />
       <StatusBar
         translucent
         style='auto'
